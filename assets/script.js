68fb5d0dddaa3f586b7b50f39d45f82d52df3242
Peter Park <peterjpark@pm.me>
2023-04-12 23:46:02 -0400

    Add logic to save cities to list

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ function getWeather(locationQuery) {
   var coordinates = [];
 
   // Convert location input to coordinates to make API call for weather data.
-  function geocodeLoc() {
+  function fetchGeocode() {
     fetch(
       `https://api.openweathermap.org/geo/1.0/direct?q=${locationQuery}&appid=${openWeatherApiKey}`
     )
@@ -12,12 +12,12 @@ function getWeather(locationQuery) {
       })
       .then((geocodedLocation) => {
         coordinates = [geocodedLocation[0].lat, geocodedLocation[0].lon];
-        fetchWeather("weather");
-        fetchWeather("forecast");
+        fetchWeatherData("weather");
+        fetchWeatherData("forecast");
       });
-  };
+  }
 
-  function fetchWeather(apiType) {
+  function fetchWeatherData(apiType) {
     fetch(
       `https://api.openweathermap.org/data/2.5/${apiType}?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=${openWeatherApiKey}`
     )
@@ -31,9 +31,10 @@ function getWeather(locationQuery) {
           console.log("temp " + weatherData.main.temp);
           console.log("feels like " + weatherData.main.feels_like);
           console.log("humidity " + weatherData.main.humidity);
-          // Get forecast.
-        } else if (apiType === "forecast") {
-          for (var i = 1; i < weatherData.list.length; i += 2) {
+        }
+        // Get 5-day forecast.
+        if (apiType === "forecast") {
+          for (var i = 1; i < weatherData.list.length - 2; i += 2) {
             if (weatherData.list[i].dt_txt.endsWith("09:00:00")) {
               console.log("Morning: " + weatherData.list[i].dt_txt);
             } else if (weatherData.list[i].dt_txt.endsWith("15:00:00")) {
@@ -44,19 +45,32 @@ function getWeather(locationQuery) {
           }
         }
       });
-  };
-
-  geocodeLoc();
+  }
+  fetchGeocode();
 }
 
-$("#today").text(dayjs().format("ddd, MMMM D"));
+// Run when document is ready.
+$(function () {
+  // Display today's date.
+  $("#today").text(dayjs().format("ddd, MMMM D"));
 
-// Get location input.
-$("#location-form").on("submit", function (e) {
-  e.preventDefault();
-  getWeather($("#location-input").val().trim());
+  // Get location input.
+  $("#location-form").on("submit", function (e) {
+    e.preventDefault();
+
+    //TODO: ADD VALIDATION.
+    getWeather($("#location-input").val().trim());
+
+    // Add searched location to saved cities.
+    $("#saved-cities").append(
+      `<li><button class="saved-city">${$(
+        "#location-input"
+      ).val()}</button></li>`
+    );
+  });
+
+  // Event delegation to handle newly saved cities.
+  $("#saved-cities").on("click", "button", function (e) {
+    getWeather(e.target.textContent);
+  });
 });
-
-$(".saved-city").on("click", function (e) {
-  getWeather(e.target.textContent);
-})
