5dfb7f87b22f0ec0b3930ac27159110c9593e660
Peter Park <peterjpark@pm.me>
2023-04-16 23:43:24 -0400

    Add logic to populate today's weather

--- assets/script.js
+++ assets/script.js
@@ -18,78 +18,117 @@ function getWeather(locationQuery) {
   }
 
   function fetchWeather(apiType) {
+    var weatherCurrent = {};
+    var weatherForecast = [{}, {}, {}, {}, {}];
+
     fetch(
-      `https://api.openweathermap.org/data/2.5/${apiType}?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=${openWeatherApiKey}`
+      `https://api.openweathermap.org/data/2.5/${apiType}?lat=${coordinates[0]}&lon=${coordinates[1]}&units=metric&appid=${openWeatherApiKey}`
     )
       .then((response) => {
         return response.json();
       })
-      .then((weatherData) => {
+      .then((data) => {
         // Get current weather.
         if (apiType === "weather") {
-          alert(weatherData.name);
-          console.log("temp " + weatherData.main.temp);
-          console.log("feels like " + weatherData.main.feels_like);
-          console.log("humidity " + weatherData.main.humidity);
+          // Populate selected city and today's date.
+          $("#current-city").append(`<i class="fa-solid fa-tree-city me-3"></i>${data.name}, ${data.sys.country}`);
+          $("#today").append(`<i class="fa-solid fa-calendar-day me-3"></i>${dayjs().format("ddd, MMMM D")}`);
+
+          // Populate today's weather.
+          $("#today-card").append(`
+            <div class="row">
+              <div class="col d-flex justify-content-around align-items-center">
+                <span id="current-temp" class="display-2">${Math.round(data.main.temp)}℃</span>
+                <ul>
+                  <li><i class="fa-solid fa-child me-1"></i>Feels like: ${Math.round(data.main.feels_like)}℃</li>
+                  <li><i class="fa-solid fa-temperature-empty me-1"></i>Low: ${Math.round(data.main.temp_min)}℃</li>
+                  <li><i class="fa-solid fa-temperature-full me-1"></i>High: ${Math.round(data.main.temp_max)}℃</li>
+                </ul>
+                <i class="fa-solid fa-cloud display-2 me-3"></i>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <hr>
+                <ul class="d-flex justify-content-around">
+                  <li><i class="fa-solid fa-wind me-1"></i>Wind speed: ${Math.round(data.wind.speed)}km/h</li>
+                  <li><i class="fa-solid fa-water me-1"></i>Humidity: ${Math.round(data.main.humidity)}%</li>
+                  <li><i class="fa-solid fa-cloud-sun me-1"></i>Sunset: ${dayjs.unix(data.sys.sunset).format("h:mm A")}</li>
+                </ul>
+              </div>
+            </div>
+          `)
         }
         // Get 5-day forecast.
         if (apiType === "forecast") {
-          for (var i = 1; i < weatherData.list.length - 2; i += 2) {
-            if (weatherData.list[i].dt_txt.endsWith("09:00:00")) {
-              console.log("Morning: " + weatherData.list[i].dt_txt);
-            } else if (weatherData.list[i].dt_txt.endsWith("15:00:00")) {
-              console.log("Afternoon: " + weatherData.list[i].dt_txt);
-            } else if (weatherData.list[i].dt_txt.endsWith("21:00:00")) {
-              console.log("Evening: " + weatherData.list[i].dt_txt);
+          for (var i = 1; i < data.list.length; i += 2) {
+            // Morning forecasts
+            if (data.list[i].dt_txt.endsWith("09:00:00")) {
+              console.log("Morning: " + data.list[i].dt_txt);
+              //format("YYYY-MM-DD HH:mm:ss")
+            }
+            // Afternoon forecasts
+            if (data.list[i].dt_txt.endsWith("15:00:00")) {
+              console.log("Afternoon: " + data.list[i].dt_txt);
+            }
+            // Evening forecasts
+            if (data.list[i].dt_txt.endsWith("21:00:00")) {
+              console.log("Evening: " + data.list[i].dt_txt);
             }
           }
         }
       });
   }
 
-  function printWeather() {
+  function printWeather() {}
+
+  function printForecast() {
     // Populate forecast cards.
     for (var i = 1; i < $(".forecast").length + 1; i++) {
-     $(`.forecast:nth-child(${i})`).append(`
-     <h4>${dayjs().format("ddd, MMMM D")}</h4>
+      $(`.forecast:nth-child(${i})`).append(`
+     <h4>${dayjs().add(i, "day").format("ddd, MMMM D")}</h4>
      <ul>
-       <li>Temperature: </li>
-       <li>Feels like: </li>
-       <li>Humidity: </li>
+       <li>Morning: </li>
+       <li>Afternoon: </li>
+       <li>Evening: </li>
      </ul>
      `);
-   }
- }
+    }
+  }
   fetchGeocode();
+  printForecast();
 }
-
-
 
 // Run when document is ready.
 $(function () {
-  // Display today's date.
-  $("#today").text(dayjs().format("ddd, MMMM D"));
-
   // Get location input.
   $("#location-form").on("submit", function (e) {
     e.preventDefault();
-
     //TODO: ADD VALIDATION.
     getWeather($("#location-input").val().trim());
 
     // Add searched location to saved cities.
     $("#saved-cities").append(
-      `<li>
-        <button class="saved-city">
-        ${$("#location-input").val()}
+      // `<li class="saved-city d-flex justify-content-between my-1">
+      // <button class="btn btn-outline-warning border-0">${$("#location-input").val()}</button>
+      // <button class="delete-city btn btn-outline-danger border-0"><i class="fa-solid fa-delete-left"></i></button>
+      // </li>`
+
+      `<li class="saved-city d-flex justify-content-between">
+        <button class="dropdown-item">
+          ${$("#location-input").val()}
+        </button>
+        <button class="delete-city btn btn-outline-danger border-0">
+          <i class="fa-solid fa-delete-left"></i>
         </button>
       </li>`
     );
   });
-
 
   // Event delegation to handle newly saved cities.
   $("#saved-cities").on("click", "button", function (e) {
     getWeather(e.target.textContent);
   });
 });
+
+var pexelsApiKey = "yrsB9C7fdv4f4SKZW0x33yLQTI5K0aQRyMFY6UUZPvHFdOGKiltYHvC6";
